c84c86c8d3a9fa5785236f8aa1ff602e08168b0a
phai-panda <kittipong.p@arise.tech>
2026-09-07 12:32:31 +0700

    config: externalize PIN attempt limit

--- features/welcome/welcome.constants.ts
+++ features/welcome/welcome.constants.ts
@@ -1,4 +1,5 @@
 const DEFAULT_PLAYER_NAME_MAX_LENGTH = 20;
+const DEFAULT_MAX_PLAYER_PIN_ATTEMPTS = 20;
 
 const configuredPlayerNameMaxLength = Number(process.env.NEXT_PUBLIC_PLAYER_NAME_MAX_LENGTH);
 
@@ -7,4 +8,9 @@ export const PLAYER_NAME_MAX_LENGTH = Number.isInteger(configuredPlayerNameMaxLe
   ? configuredPlayerNameMaxLength
   : DEFAULT_PLAYER_NAME_MAX_LENGTH;
 
-export const MAX_PLAYER_PIN_ATTEMPTS = 20;
+const configuredMaxPlayerPinAttempts = Number(process.env.MAX_PLAYER_PIN_ATTEMPTS);
+
+export const MAX_PLAYER_PIN_ATTEMPTS = Number.isInteger(configuredMaxPlayerPinAttempts)
+  && configuredMaxPlayerPinAttempts > 0
+  ? configuredMaxPlayerPinAttempts
+  : DEFAULT_MAX_PLAYER_PIN_ATTEMPTS;
